bf6a8bff81718e7689ccd4ba278ea79f76c2f824
Juan A. Biforcos Amor <biforcosamor@gmail.com>
2026-05-03 22:15:49 +0200

    chore: exclude .worktrees from Vitest test discovery

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,6 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
     globals: true,
+    exclude: ['**/node_modules/**', '**/.worktrees/**'],
   },
 })
